2db82cbf3677b564b2d5376186f75d8ba9a67de7
dbruchwald <d.bruchwald@campus.tu-berlin.de>
2023-05-15 16:39:45 +0200

    data transfer to thread via queue

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import threading
 import queue
 
 def thread_runner(id):
-    print(id)
+    while True:
+        while not input_queues[id].empty():
+            data = input_queues[id].get()
+            print(f"Processing data: {data}")
 
 if __name__ == '__main__':
     nmbThreads = 4
@@ -11,11 +14,13 @@ if __name__ == '__main__':
     threads = []
     input_queues = []
     for i in range(nmbThreads):
-        threads.append(threading.Thread(target=thread_runner(i)))
         input_queues.append(queue.Queue())
+        threads.append(threading.Thread(target=thread_runner, args=(i,)))
 
     for i in range(nmbThreads):
         threads[i].start()
+
+    input_queues[0].put("hey first thread")
 
     for i in range(nmbThreads):
         threads[i].join()
