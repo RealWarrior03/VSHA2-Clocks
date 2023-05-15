4a70413afc8cf96451664f99bb68ee1531c41b09
dbruchwald <d.bruchwald@campus.tu-berlin.de>
2023-05-15 17:05:32 +0200

    implemented message class which can store extra info and adjusted data transfer

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import threading
 import queue
 
+EXTERNAL_FLAG = 0
+INTERNAL_FLAG = 1
+
+#class for exchanging data
+class Message:
+    def __init__(self, flag, payload):
+        self.flag = flag
+        self.payload = payload
+
+
 def thread_runner(id):
     while True:
-        while not input_queues[id].empty():
-            data = input_queues[id].get()
-            print(f"Processing data: {data}")
+        while not input_queues[id].empty():  # checks for new messages in input_queue
+            data = input_queues[id].get()  # get data out of input_queue
+
+            # process data
+            payload = data.payload
+            print(f"Processing data: {payload}")
             print(f"thread id: {id} ")
+
+
+def message_sequencer_runner(nmbThreads):
+    print("Test")
+
 
 if __name__ == '__main__':
     nmbThreads = 4
@@ -14,15 +32,18 @@ if __name__ == '__main__':
     # Erstelle zwei Threads
     threads = []
     input_queues = []
+    # threads and queues are generated and can be access through arrays
     for i in range(nmbThreads):
         input_queues.append(queue.Queue())
         threads.append(threading.Thread(target=thread_runner, args=(i,)))
 
-    for i in range(nmbThreads):
+    for i in range(nmbThreads):  # threads are started
         threads[i].start()
 
-    input_queues[0].put("hey first thread")
-    input_queues[2].put("hello second thread")
+    # data transfer test
+    m = Message(EXTERNAL_FLAG, "hey first thread")
+    input_queues[0].put(m)
 
-    for i in range(nmbThreads):
+
+    for i in range(nmbThreads):  # threads are joined in the end to ensure every thread is finished
         threads[i].join()
