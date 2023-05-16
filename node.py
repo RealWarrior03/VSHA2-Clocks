354a1ddee31f308b7bfb920539ed34a9fb15127b
tobi <kunzi@campus.tu-berlin.de>
2023-05-16 14:40:26 +0200

    Changes by lukas

--- node.py
+++ node.py
@@ -4,8 +4,13 @@ import messages
 
 #class Node contains Input-Queue, Array to store history of incoming messages
 class Node:
-    inbox = queue.Queue()
-    recieved_messages = []
+
+
+    def __init__(self,nodeID):
+        inbox = queue.Queue()
+        recieved_messages = []
+        self.nodeID=nodeID
+
 
     def input_queue(self):
         return self.inbox
